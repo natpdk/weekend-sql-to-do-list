e11b85c9e4fa31ce628eee95e2b70981ea4c8031
Nat Koch <nat.pd.koch@gmail.com>
2024-11-17 12:16:41 -0800

    Added get a post routes to todos router.

--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
+//GET route in order to get all the todos from the database:
 
+router.get('/', (req, res) => {
+    const queryText = 'SELECT * FROM todos ORDER by id;';
+    pool.query(queryText)
+        .then((result) => res.json(result.rows))
+        .catch((err) => {
+            console.error('Error getting the todos from the database.', err);
+            res.sendStatus(500);
+    });
+});
 
+//POST route to add a new todo:
+router.post('/', (req, res) => {
+    const task = req.body;
+    const queryText = 'INSERT INTO todos (task) VALUES ($1);';
+    pool.query(queryText, [task] )
+        .then(() => res.sendStatus(201))
+        .catch((err) => {
+            console.error('Error adding new todo.', err);
+            res.sendStatus(500);
+        });
+});
 
 module.exports = router;
