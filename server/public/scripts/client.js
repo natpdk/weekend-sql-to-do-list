7888b213e1eb0bd0056da492cdf5f7e446475370
Nat Koch <nat.pd.koch@gmail.com>
2024-11-17 12:38:17 -0800

    Added function to client to get the todos.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log('JS is sourced!');
+
+getTodos();
+
+function getTodos() {
+    axios.get('/todos')
+        .then((response) => {
+            const todos = response.data;
+            const todoList = document.getElementById('todos');
+            todoList.innerHTML = ''; //Clear the current list???
+        })
+}
